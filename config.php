013a04e2095ae45740e38988bb6152a71d6d34df
kptest <kprakasht@yahoo.co.in>
2016-06-29 22:33:07 +0530

    Signed-off-by: kptest <kprakasht@yahoo.co.in>

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'osclass');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'ec2-54-82-171-193.compute-1.amazonaws.com');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/osclass/');
+
+define('WEB_PATH', 'http://ec2-54-82-171-193.compute-1.amazonaws.com/osclass/');
+
+?>

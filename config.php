8ee573dca8e0d40ee6d93bd07f38d27a47e7b2b1
kptest <kprakasht@yahoo.co.in>
2016-06-30 00:19:37 +0530

    Signed-off-by: kptest <kprakasht@yahoo.co.in>

--- config.php
+++ config.php
@@ -12,10 +12,10 @@ define('DB_NAME', 'osclass');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'testos!@3t');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'ubuntu@ec2-54-82-171-193.compute-1.amazonaws.com');
+define('DB_HOST', 'ec2-54-82-171-193.compute-1.amazonaws.com');
 
 /** Database Table prefix */
 define('DB_TABLE_PREFIX', 'oc_');
